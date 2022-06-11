797bb1ad867e991f239ede05ad8b8f414d745fcc
thomas lefranc <thomas.lefranc@polytech-lille.net>
2022-06-11 13:09:30 +0200

    récupération des données disques

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-	printf("bonjour\n");
+//lsblk -f pour récuperer les infos sur les disks et l'UUID
+
+int main(int argc,char * argv[]){
+	for(int i = 0; i< argc; i++){
+		printf("%s\n",argv[i]);
+	}
+	
+	FILE * f = popen("lsblk -f","r");
+	char ligne[512] = "ok";
+
+	while(fgets(ligne,512,f)){	
+		printf("%s\n",ligne);
+	}
 }
