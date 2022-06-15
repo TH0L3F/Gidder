f367e018e743df989ce95e70a0cc5d1eaaae6b39
thomas lefranc <thomas.lefranc@polytech-lille.net>
2022-06-15 14:20:09 +0200

    maj

---
+++ fonction.h
@@ -0,0 +1,9 @@
+struct disque{
+	char position[256];
+	char type[4];	
+	char vers[16];
+	char label[128];
+	char uuid[64];
+	int space;
+	char mountpoint[256];		
+};	
--- main.c
+++ main.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-//lsblk -f pour récuperer les infos sur les disks et l'UUID
+#include <fonction.h>
 
 int main(int argc,char * argv[]){
 	for(int i = 0; i< argc; i++){
 		printf("%s\n",argv[i]);
 	}
 	
-	FILE * f = popen("lsblk -f","r");
+	FILE * f = popen("lsblk -rfpb","r");
 	char ligne[512] = "ok";
+	struct disque * d;	
 
 	while(fgets(ligne,512,f)){	
-		printf("%s\n",ligne);
+		printf("%s",ligne);
+		d = malloc(sizeof(struct disque));
+		strcpy(d->position,strtok(ligne," "));
+		strcpy(d->type,strtok(NULL," "));
+		strcpy(d->vers,strtok(NULL," "));
+		strcpy(d->label,strtok(NULL," "));
+		strcpy(d->uuid,strtok(NULL," "));
+		d->space = atoi(strtok(NULL," "));	
+		strtok(NULL," ");
+		strcpy(d->mountpoint,strtok(NULL,"\n"));
+		printf("%s\n",d->uuid);
 	}
 }
